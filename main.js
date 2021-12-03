293ced4552cdeb26b00616036e2a3ffca03e5315
andriykhomik <andriykhomik1992@gmail.com>
2021-12-04 00:13:50 +0200

    added some features for improving validation

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
 const codes = document.querySelectorAll('.code')
 
-codes[0].focus()
+let i = 0;
+
+codes[i].focus();
+
 
 codes.forEach((code, idx) => {
-    code.addEventListener('keydown', (e) => {
-        if(e.key >= 0 && e.key <=9) {
-            codes[idx].value = ''
-            setTimeout(() => codes[idx + 1].focus(), 10)
+    code.addEventListener('keydown',  (e) => {
+
+        setTimeout(()=> {
+            codes.forEach(code => {
+
+                if (code.value.length > 1){
+                    code.value.split('').map((item, idx) => {
+                        i = idx;
+                        codes[i].value = item;
+                        setTimeout(() => codes[i].focus(), 10);
+                    });
+                }
+            });
+        }, 20)
+
+        if(e.key >= 0 && e.key <= 9) {
+            i = idx;
+            if (i >= codes.length - 1){
+                i = codes.length - 1;
+                codes[i].value = '';
+                setTimeout(() => codes[i].focus(), 10);
+            } else {
+                codes[i].value = ''
+                setTimeout(() => codes[i + 1].focus(), 10);
+            }
         } else if(e.key === 'Backspace') {
-            setTimeout(() => codes[idx - 1].focus(), 10)
+            if (i <= 1){
+                setTimeout(() => codes[i].focus(), 10);
+            }else {
+                setTimeout(() => codes[i - 1].focus(), 10);
+            }
         }
+        i = idx;
+        codes[i].focus();
     })
 })
 
